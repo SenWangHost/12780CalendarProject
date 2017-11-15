66409a34681f0fe8a3a96dcf0bc4ed9e261b5530
SenWangHost <senwang1992010@gmail.com>
2017-11-15 00:16:15 -0500

    Add the login application and achieve url mapping to this application

--- calendarapp/login/views.py
+++ calendarapp/login/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import get_template
 
 # Create your views here.
+def login(request):
+    t = get_template('login.html')
+    html = t.render()
+    return HttpResponse(html)
