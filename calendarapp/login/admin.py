fa9ec8b49a8dce514cc8781df37ba01c28c0af15
SenWangHost <senwang1992010@gmail.com>
2017-11-15 13:32:24 -0500

    Add user model to the project, just for learning purpose.

--- calendarapp/login/admin.py
+++ calendarapp/login/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password', 'firstname', 'lastname')
+    search_fields = ('firstname','lastname')
+
+admin.site.register(User, UserAdmin)
--- calendarapp/login/models.py
+++ calendarapp/login/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length = 30)
+    password = models.CharField(max_length = 30)
+    firstname = models.CharField(max_length = 50)
+    lastname = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return u'%s %s' % (self.firstname, self.lastname)
