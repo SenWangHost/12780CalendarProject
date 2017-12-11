d95943dfa54feb0335d044ac9aac6667aea4331c
SenWangHost <senwang1992010@gmail.com>
2017-12-11 00:07:39 -0500

    Change the background image and the link after register page.

--- calendarapp/static/js/register.js
+++ calendarapp/static/js/register.js
@@ -81,7 +81,7 @@ function register() {
                 return;
             }
             if (this.responseText == "true") {
-                window.location.href = "http://localhost:8000/todolist/";
+                window.location.href = "http://localhost:8000/calendar/";
                 return;
             }
         }
